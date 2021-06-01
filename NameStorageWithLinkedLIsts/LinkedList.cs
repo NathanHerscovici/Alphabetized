c61b1481426a3163d80f1571678cf86a6090ec14
Nathan Herscovici <nherscovici@ksu.edu>
2021-06-01 10:17:09 -0500

    Add project files.

---
+++ NameStorageWithLinkedLIsts/LinkedList.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Win32;
+using System.Windows;
+using System.IO;
+
+namespace NameStorageWithLinkedLIsts
+{
+    public class Node
+    {
+        public String value;
+        public Node next;
+        public Node previous;
+    }
+    public class LinkedList
+    {
+
+        private Node head;
+
+        public LinkedList()
+        {
+            head = new Node();
+        }
+
+        public void add(String toBeStored)
+        {
+            bool notDone = true;
+            if (head.value == null)
+            {
+                Node toAdd = new Node();
+                toAdd.value = toBeStored;
+                head = toAdd;
+            }
+            else
+            {
+                Node search = head;
+                Node searchBehind = head;
+                String currentData;
+                bool searchBehindNeedsMoved = false;
+
+                while (notDone)
+                {
+
+                    currentData = search.value;
+                    if (currentData[0] > toBeStored[0])
+                    {
+                        if (search == head)
+                        {
+                            Node toAdd = new Node();
+                            toAdd.value = toBeStored;
+                            toAdd.next = head;
+                            head.previous = toAdd;
+                            head = toAdd;
+                            notDone = false;
+                        }
+                        else
+                        {
+
+                            Node toAdd = new Node();
+                            toAdd.value = toBeStored;
+                            toAdd.next = search;
+                            toAdd.previous = searchBehind;
+                            searchBehind.next = toAdd;
+                            search.previous = toAdd;
+                            notDone = false;
+
+                        }
+                    }
+                    else if (currentData[0] == toBeStored[0])
+                    {
+                        bool stillTheSame = true;
+                        int i = 1;
+                        while (stillTheSame == true)
+                        {
+                            if (currentData[i] == toBeStored[i])
+                                i++;
+                            else
+                                stillTheSame = false;
+                        }
+
+                        if (currentData[i] > toBeStored[i])
+                        {
+
+                            if (search != head)
+                            {
+                                Node toAdd = new Node();
+                                toAdd.value = toBeStored;
+                                toAdd.next = search;
+                                toAdd.previous = searchBehind;
+                                searchBehind.next = toAdd;
+                            }
+                            else
+                            {
+                                Node toAdd = new Node();
+                                toAdd.value = toBeStored;
+                                toAdd.next = head;
+                                head.previous = toAdd;
+                                head = toAdd;
+                                notDone = false;
+                            }
+                        }
+
+                    }
+                    else
+                    {
+                        if (search.next == null)
+                        {
+                            Node toAdd = new Node();
+                            toAdd.value = toBeStored;
+                            search.next = toAdd;
+                            toAdd.previous = search;
+                            notDone = false;
+                        }
+                        else
+                        {
+                            search = search.next;
+                            if (searchBehindNeedsMoved)
+                                searchBehind = searchBehind.next;
+                            else
+                                searchBehindNeedsMoved = true;
+                        }
+                    }
+
+                }
+
+            }
+
+            return;
+
+        }
+
+        public String writeInOrder()
+        {
+
+            Node runThrough = new Node();
+            runThrough = head;
+            String building = "";
+
+            while (runThrough.next != null)
+            {
+                building = building + " " + runThrough.value;
+                runThrough = runThrough.next;
+            }
+            building = building + " " + runThrough.value;
+
+
+            return building;
+
+        }
+
+        public String writeInReverseOrder()
+        {
+
+            Node runThrough = new Node();
+            runThrough = head;
+            String building = "";
+
+            while(runThrough.next != null)
+            {
+                runThrough = runThrough.next;
+            }
+
+            while(runThrough.previous != null)
+            {
+                building += " " + runThrough.value;
+                runThrough = runThrough.previous;
+            }
+
+            building += " " + runThrough.value;
+
+            return building;
+
+        }
+
+    }
+}
---
+++ NameStorageWithLinkedLIsts/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;
+
+namespace NameStorageWithLinkedLIsts
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        String test;
+        LinkedList listOfNames = new LinkedList();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                StreamReader sr = new StreamReader(openFileDialog.FileName);
+                test = sr.ReadToEnd();
+                splitUp(test);
+                fileNameBox.Text = openFileDialog.FileName;
+
+                //listOfNames.add(test);
+                //MessageBox.Show(listOfNames.writeInOrder());
+            }
+
+        }
+
+        public void splitUp(String bigString)
+        {
+            String wordToAdd = "";
+            for (int i = 0; i < bigString.Length; i++)
+            {
+                if (bigString[i] == ' ')
+                {
+                    listOfNames.add(wordToAdd);
+                    wordToAdd = "";
+                }
+                else
+                {
+                    wordToAdd += bigString[i];
+                }
+            }
+            listOfNames.add(wordToAdd);
+
+        }
+
+        private void alphabeticalButton_Click(object sender, RoutedEventArgs e)
+        {
+            outputBox.Text = listOfNames.writeInOrder();
+        }
+
+        private void reverseAlphabeticalButton_Click(object sender, RoutedEventArgs e)
+        {
+            outputBox.Text = listOfNames.writeInReverseOrder();
+        }
+
+    }
+}
